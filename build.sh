05f5ff5f65d68475426581fc053632c15b2f2125
Jacob Sapoznikow <500074@bsd48.org>
2022-11-03 06:35:12 +0000

    CI

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo dpkg-deb --build --root-owner-group deb
-mv deb.deb godot-stable-v3.5.1-mono.deb
+sudo dpkg-deb --verbose --debug --build --root-owner-group deb
+mv -f deb.deb godot-stable-v3.5.1-mono.deb
